691d9e54032957227ed15baed6625b9738fc48c1
DevTare3 <tarik.mujezinovicbiz@gmail.com>
2024-11-09 20:40:50 +0100

    Still doing it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -57,7 +57,7 @@
   <div>
     <div>
       <img
-        class="relative top-4 bottom-0 right-2"
+        class="relative top-4 bottom-0 right-2 left-2"
         src={pentagon}
         alt="background layer of the game"
       />
